654ded967865065e522c1dc25fd2758578f109c0
Nivash_sharma <nivasmathu@gmail.com>
2024-10-07 01:51:53 +0530

    make folders

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.sidebar.title("Mind Map Generator")
+page = st.sidebar.selectbox("Go to", ["Home", "Generate Mind Map", "Customize Mind Map", "Previous Mind Maps", "Export & Download", "Settings"])
+
+if page == "Home":
+    import pages.home
+elif page == "Generate Mind Map":
+    import pages.generate_mindmap
+elif page == "Customize Mind Map":
+    import pages.customize_mindmap
+elif page == "Previous Mind Maps":
+    import pages.previous_mindmaps
+elif page == "Export & Download":
+    import pages.export_download
+elif page == "Settings":
+    import pages.settings
